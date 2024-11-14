ba838a66ea847920e3fd83cc06513dd0bffeb2eb
talharpaz <tal.harpaz01@gmail.com>
2024-11-14 08:47:54 +0200

    calcpi

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,36 @@
 // Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
-	    // Replace this comment with your code
+
+	 int input = Integer.parseInt(args[0]);
+	 if (input <= 1) {
+		System.out.println("Value not valid");
+		return;	
+	 }
+
+	 double sum = 1;
+	 double denominator = 3;
+	 
+	 
+	 for ( int i=1; i < input; i++){
+
+		
+		if (i%2 == 0) {
+			sum = sum + 1.0/denominator;
+			denominator = denominator + 2;
+		} 
+		else {
+			sum = sum - 1.0/denominator;
+			denominator = denominator + 2;
+		}
+	 }
+	 System.out.println("pi according to Java: 3.141592653589793");
+	 System.out.println("pi, approximated: " + sum * 4.0);
 	}
 }
+/* % java CalcPi 10
+pi according to Java: 3.141592653589793
+pi, approximated: 3.0418396189294032
+% java CalcPi 1000
+pi according to Java: 3.141592653589793
+pi, approximated: 3.140592653839794 */
--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,24 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        // Replace this comment with your code
+        
+        int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
+        int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
+        int additionalMin = Integer.parseInt(args[1]);
+
+        int totalMin = hours * 60 + minutes + additionalMin;
+        int totalHours = totalMin/60;
+        int newHours = totalHours%24;
+        int newMinuets = totalMin -( totalHours * 60);
+        
+
+        String finalHours = (newHours < 10) ? "0" + newHours : String.valueOf(newHours);
+        String finalMinutes = (newMinuets < 10) ? "0" + newMinuets : String.valueOf(newMinuets);
+
+        System.out.println(finalHours + ":" + finalMinutes);
+        
     }
 }
+    
+
+
+
