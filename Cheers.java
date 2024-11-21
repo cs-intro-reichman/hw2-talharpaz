2a8c677286b2d6576ed5e3a0ac05c37e4bb760fb
talharpaz <tal.harpaz01@gmail.com>
2024-11-21 17:53:36 +0200

    fix1

--- Cheers.java
+++ Cheers.java
@@ -13,12 +13,13 @@ public class Cheers {
                 System.out.println("Give me an " + input.charAt(i)  +": " +input.charAt(i) + "!" );
                 } 
                 else {
-                System.out.println("Give me a " + input.charAt(i)  +": " +input.charAt(i) + "!" );
+                System.out.println("Give me a  " + input.charAt(i)  +": " +input.charAt(i) + "!" );
                 }
 
                 i++;
                 
             }
+                System.out.println("What does that spell?" );
             
             int j=0;
             while (j < count) {
@@ -27,4 +28,13 @@ public class Cheers {
         }
 }
 }
-
+/*Give me an R: R!
+Give me a U: U!
+Give me an N: N!
+Give me an I: I!
+What does that spell?
+RUNI!!!
+RUNI!!!
+RUNI!!!
+RUNI!!!
+RUNI!!! */
